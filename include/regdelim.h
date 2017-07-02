adbf41c03d04b7cd8e56aebc87888ac03dfc734e
Edson Y. Toma <edson.toma@usp.br>
2017-07-02 13:06:11 -0300

    insert_worstFit cabecalho de funcao

--- include/regdelim.h
+++ include/regdelim.h
@@ -10,6 +10,8 @@ int remove_record_no_sort(int ticket, char *file_bin, INDEX ***vector, int *size
 int remove_record_ascending_sort(int ticket, char *file_bin, INDEX ***vector, int *size);
 int remove_record_descending_sort(int ticket, char *file_bin, INDEX ***vector, int *size);
 
+int insert_worstFit(char *, INDEX ***, int *, REG *);
+
 void show_list(char *filename);
 
 #endif
